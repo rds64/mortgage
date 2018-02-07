9faae8469cfb27eac720c0feb2421deabeb5b376
Dan Smith <rds2@gwmail.gwu.edu>
2018-02-07 01:59:46 -0500

    added null object type method for amortize; added more tests for amortize

--- R/amortize.R
+++ R/amortize.R
@@ -7,15 +7,31 @@
 #' @imoortFrom purr unnest
 #' @export
 #' 
-amortize <- function(.data, principal = 'principal', term = 'term', rate = 'rate', id = NULL) {
-        UseMethod("amortize")
+amortize <- function(.data = NULL, principal = 'principal', term = 'term', rate = 'rate', id = NULL) {
+        UseMethod("amortize", .data)
 }
+#.
+#' @export
+amortize.default <- function(.data = NULL, principal = 'principal', term = 'term', rate = 'rate', id = NULL) {
+    
+    if (is.null(id)) {
+        id <- c(1:length(principal))
+        message("adding id column 1...", length(principal))
+    } else {
+        id <- id
+    }
+    
+    amort_df <- do.call(rbind, Map(pryr::partial(.Call, "_mortgage_amortize"), principal, term, rate / 12))
+    amort_df <- cbind(id, tibble::as.tibble(amort_df))
+    tibble::as.tibble(amort_df)
+}
+#'
 #' @export
 amortize.data.frame <- function(.data, principal = 'principal', term = 'term', rate = 'rate', id = NULL) {
     principal <- tidyselect::vars_pull(names(.data), !! rlang::enquo(principal))
     term      <- tidyselect::vars_pull(names(.data), !! rlang::enquo(term))   
     rate      <- tidyselect::vars_pull(names(.data), !! rlang::enquo(rate))   
-   
+    
     if (is.null(id)) {
         id <- c(1:nrow(.data))
         message("adding id column 1...", nrow(.data))
--- tests/testthat/test_amortize.R
+++ tests/testthat/test_amortize.R
@@ -1,36 +1,40 @@
 testthat::context("amortizeing a data.frame of lonas")
 
 test_df <- data.frame(
+    id = "A",
     principal = 100000,
     term = 360,
     rate = .04
 )
 
 testthat::test_that("amortizing one row data frame with standard names", {    
-    
    amortize(test_df, principal = principal, term = term, rate = rate) 
 })
 
 testthat::test_that("amortizing one row data frame with standard names and default arguments", {    
-    
    amortize(test_df)
 })
 
+testthat::test_that("amortizing one loan id from data", {    
+   amortize(test_df, id = id)
+   amortize(test_df, id = "id")
+})
+
 test_df <- data.frame(
+    id = c("A", "B", "C"),
     principal = c(100000, 200000, 400000),
     term = c(360, 360, 180),
     rate = c(.04, .045, .425)
 )
 
 testthat::test_that("amortizing row data with multiple rows frame with standard names", {    
-    
    amortize(test_df, principal = principal, term = term, rate = rate) 
+   amortize(test_df, principal = 'principal', term = 'term', rate = 'rate') 
 })
 
 names(test_df) <- c('p', 't', 'r')
 
 testthat::test_that("amortizing row data with multiple rows frame with standard names", {    
-    
    amortize(test_df, principal = p, term = t, rate = r) 
 })
 
@@ -41,4 +45,12 @@ testthat::test_that("unnesting amortized tibble output", {
             nrow),
         sum(test_df$t) + length(test_df$t)
     )
+})
+
+testthat::test_that("amortizing with default method", {    
+        amortize(principal = test_df$p, term = test_df$t, rate = test_df$r)
+})
+
+testthat::test_that("amortizing with default method and loan id", {    
+    amortize(principal = test_df$p, term = test_df$t, rate = test_df$r, id = test_df$id)
 })
